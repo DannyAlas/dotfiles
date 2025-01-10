2fa837dea308c764e415826d40c5a534b4cc55a8
DannyAlas <danielalas65@gmail.com>
2025-01-10 11:11:54 -0500

    astro nvim init extras

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,9 @@
+return {
+  "goolord/alpha-nvim",
+  opts = function(_, opts)
+    opts.section.header.val = {
+      "        WEEWOOWEEWOO ",
+      "DANNY'S GONNA MAKE SOME BUGS! ",
+    }
+  end,
+}
